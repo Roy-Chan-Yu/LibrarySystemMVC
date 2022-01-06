935a8bce5c9174f9d307bbb3b87ebce5ec2fab2b
育程 詹 <w983874@gmail.com>
2022-01-06 11:02:05 +0800

    git add comment out v.1.0.1

--- LibraryInformationSystem/Global.asax.cs
+++ LibraryInformationSystem/Global.asax.cs
@@ -16,6 +16,7 @@ namespace LibraryInformationSystem
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // Comment out
         }
     }
 }
